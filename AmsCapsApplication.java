e7bcff623ff6df3df04857a518462b49686415cf
ABHILASH-T-S <99649871+ABHILASH-T-S@users.noreply.github.com>
2022-04-30 11:18:07 +0530

    Add files via upload

---
+++ AmsCapsApplication.java
@@ -0,0 +1,13 @@
+package com.accionlabs.AMSCaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsCapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsCapsApplication.class, args);
+	}
+
+}
